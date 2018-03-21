af5201eca3b05e283f86934083e6e13aef47b589
Mike <mikeramzdesign@gmail.com>
2018-03-21 10:58:03 -0700

    updated js and htmnl

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 
-const NEWS_SEARCH_URL = 'https://content.guardianapis.com/search';
+const NEWS_SEARCH_URL = 'http://content.guardianapis.com/search';
 
-function getDataFromApi(callback) {
-  const settings = {
-    url: NEWS_SEARCH_URL,
-    data: {
-      q: 'business',
-      key: 'bbf4d829-c55d-4dca-b2d3-addc895cb3d2',
-      max_Results: 5
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback
-  };
+function getDataFromApi(searchTerm, callback) {
+    const settings = {
+        url: NEWS_SEARCH_URL,
+        data: {
+            q: `${searchTerm}`,
+            key: 'bbf4d829-c55d-4dca-b2d3-addc895cb3d2',
+        },
+        dataType: 'json',
+        type: 'GET',
+        success: callback
+    };
 
-  $.ajax(settings);
+    $.ajax(settings);
 }
 
 function renderResult(result) {
-        $(".js-news").append('result.results.webTitle');
+    return `
+            <div> 
+                <p>${result.webTitle}</p>
+                <p>${result.sectionName}</p>
+            </div>
+            `;
 
 }
 
-getDataFromApi();
-renderResult();
+function displayNewsData(data) {
+    const showResults = data.results.map((item, index) => renderResult(item));
+    $('.js-news').html(showResults);
+}
+
+function watchSubmit() {
+    $('.js-search-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        getDataFromApi(query, displayNewsData);
+    });
+}
+
+$(watchSubmit);
