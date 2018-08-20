2b74f5ed28ad7cfac91e2e32d830111829d3957e
Mike <mikeramzdesign@gmail.com>
2018-08-19 17:56:41 -0700

    updated content

--- main.js
+++ main.js
@@ -85,6 +85,7 @@ function displaySunData(sunData) {
     const convertedSunset = convertToPst(sunData.results.sunset);
     let sunResultText = `
             <h3> Mindful Day (Portland, OR) </h3>
+            <p><small><i>Know when sunrises and sunsets</i></small></p>
             <hr class="line">
             <div class="sun rise">Sunrise: ${convertedSunrise}</div>
             <div class="sun set">Sunset : ${convertedSunset}</div>`
